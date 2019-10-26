7fe469d421d58c5858f86b9583115facc9f3e547
Alexey Berezin <beraliv.spb@gmail.com>
2019-10-27 02:29:35 +0300

    prod: add rounded image ⚪

---
+++ components/RoundedImage.vue
@@ -0,0 +1,18 @@
+<template>
+  <img 
+    class="rounded-image"
+    :src="source"
+  />
+</template>
+
+<script>
+export default {
+  props: ['source'],
+}
+</script>
+
+<style>
+.rounded-image {
+  border-radius: 50%;
+}
+</style>
