6d1ae921a14b9a90a538c15386107c23b9e5fc1d
Alexey Berezin <beraliv.spb@gmail.com>
2019-11-03 17:34:16 +0300

    prod: add medium link 🔗 and optimise svgs 🖼️

--- pages/index.vue
+++ pages/index.vue
@@ -26,6 +26,13 @@
         iconClass="twitter-icon"
         aria-label="Visit my Twitter page"
       />
+      <logo-link
+        class="link medium"
+        href="https://medium.com/@beraliv"
+        iconClass="medium-icon"
+        aria-label="Visit my Medium blog page"
+      />
+      <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
     </footer>
   </div>
 </template>
@@ -88,6 +95,10 @@ footer {
   display: flex;
 }
 
+footer > div:last-child {
+  display: none;
+}
+
 .github-icon {
   background-image: url('/github.svg');
 }
@@ -98,5 +109,9 @@ footer {
 
 .twitter-icon {
   background-image: url('/twitter.svg');
+}
+
+.medium-icon {
+  background-image: url('/medium.svg');
 }
 </style>
