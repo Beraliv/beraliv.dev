5ec22ed98a21865a0996f1d4b5bc35d5540e9112
Alexey Berezin <beraliv.spb@gmail.com>
2021-03-08 21:04:45 +0100

    refactor: 🔄 use-dark-mode => local useDarkMode

---
+++ packages/blog/src/hooks/useDarkMode.tsx
@@ -0,0 +1,47 @@
+import { useCallback, useLayoutEffect, useState } from "react"
+
+const STORAGE_KEY = "dark"
+
+const INITIAL_DARK_MODE = (() => {
+  try {
+    const persistedDarkMode = localStorage.getItem(STORAGE_KEY)
+    if (persistedDarkMode === null) {
+      return false
+    }
+
+    return JSON.parse(persistedDarkMode)
+  } catch (error) {
+    return false
+  }
+})()
+
+const updateStorage = (darkMode: boolean) => {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(darkMode))
+}
+
+const updateDom = (darkMode: boolean) => {
+  document.body.classList.toggle("dark", darkMode)
+}
+
+type UseDarkModeReturnValue = {
+  darkMode: boolean
+  toggle: VoidFunction
+}
+
+export const useDarkMode = (): UseDarkModeReturnValue => {
+  const [darkMode, setDarkMode] = useState(INITIAL_DARK_MODE)
+
+  useLayoutEffect(() => {
+    updateStorage(INITIAL_DARK_MODE)
+    updateDom(INITIAL_DARK_MODE)
+  }, [])
+
+  const toggle = useCallback(() => {
+    const nextDarkMode = !darkMode
+    updateStorage(nextDarkMode)
+    updateDom(nextDarkMode)
+    setDarkMode(nextDarkMode)
+  }, [darkMode, setDarkMode])
+
+  return { darkMode, toggle }
+}
