2b01c7b1837f02a14ec27330a216605a5fb735b1
Alexey Berezin <beraliv.spb@gmail.com>
2021-03-10 12:41:40 +0100

    fix: 🧪 NotFoundPage types

--- packages/blog/src/pages/404.tsx
+++ packages/blog/src/pages/404.tsx
@@ -5,10 +5,14 @@ import { Seo } from "../components/Seo"
 import { NotFoundQuery } from "../types/generated"
 
 const NotFoundPage = ({ data, location }: PageProps<NotFoundQuery>) => {
-  const siteTitle = data.site.siteMetadata.title
+  if (!data.site?.siteMetadata?.title) {
+    throw new Error("Cannot find config.siteMetadata.title in gatsby-config.ts")
+  }
+
+  const { title } = data.site.siteMetadata
 
   return (
-    <Layout location={location} title={siteTitle}>
+    <Layout location={location} title={title}>
       <Seo title="404: Not Found" pathname={location.pathname} />
       <h1>404: Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
