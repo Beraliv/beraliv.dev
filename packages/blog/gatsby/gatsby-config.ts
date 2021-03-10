0b86690bc58a5e54d364e45316ea14a365696f48
Alexey Berezin <beraliv.spb@gmail.com>
2021-03-10 20:55:27 +0100

    build: ➕ add tailwind: true

--- packages/blog/gatsby/gatsby-config.ts
+++ packages/blog/gatsby/gatsby-config.ts
@@ -81,6 +81,11 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
     `gatsby-plugin-postcss`,
-    `gatsby-plugin-purgecss`,
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        tailwind: true,
+      },
+    },
   ],
 }
