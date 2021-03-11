52cdad98d9d391dd7fd5e6680da6bc7792fd3ee8
Alexey Berezin <beraliv.spb@gmail.com>
2021-03-11 16:54:23 +0100

    feat: ➕ add trackingId

--- packages/blog/gatsby/gatsby-config.ts
+++ packages/blog/gatsby/gatsby-config.ts
@@ -58,7 +58,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `G-XYPXR2VBMM`,
       },
     },
     `gatsby-plugin-feed`,
