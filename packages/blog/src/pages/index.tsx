4f1b6b227d2eb7e3d9f74f2443828d7883f2b177
Alexey Berezin <beraliv.spb@gmail.com>
2021-03-17 23:11:46 +0100

    refactor: 🔄 title, date => date, title

--- packages/blog/src/pages/index.tsx
+++ packages/blog/src/pages/index.tsx
@@ -54,12 +54,12 @@ const BlogIndex = ({ data, location }: PageProps<BlogIndexQuery>) => {
             <li className="post-list-item" key={post.fields.slug}>
               <article itemScope itemType="http://schema.org/Article">
                 <header>
+                  <small>{post.frontmatter.date}</small>
                   <h2>
                     <Link to={post.fields.slug} itemProp="url">
                       <span itemProp="headline">{title}</span>
                     </Link>
                   </h2>
-                  <small>{post.frontmatter.date}</small>
                 </header>
                 <section>
                   {image?.childImageSharp?.gatsbyImageData && (
