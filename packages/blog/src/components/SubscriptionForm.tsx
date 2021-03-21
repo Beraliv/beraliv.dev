7c5996d143ec534b1b7648ec854a7988ef0615df
Alexey Berezin <beraliv.spb@gmail.com>
2021-03-21 18:57:47 +0100

    refactor: ❌ remove useless useEffect import

--- packages/blog/src/components/SubscriptionForm.tsx
+++ packages/blog/src/components/SubscriptionForm.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react"
+import React, { useCallback, useState } from "react"
 import { Formik, Form, Field, ErrorMessage } from "formik"
 import * as Yup from "yup"
 
