163af58ae4a4ed2b448f8de3707f9b27f8adb066
Alexey Berezin <beraliv.spb@gmail.com>
2021-03-22 09:23:08 +0100

    feat: ➕ add animation on hover

--- packages/portfolio/components/LogoLink.vue
+++ packages/portfolio/components/LogoLink.vue
@@ -1,21 +1,24 @@
 <template>
-  <a 
-    class="logo-link"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
+  <a class="logo-link" target="_blank" rel="noopener noreferrer">
     <div :class="iconContainerClass">
-      <img class='icon' :class="iconClass" :src="iconSrc" />
-      <span v-if="title">{{title}}</span>
+      <img class="icon" :class="iconClass" :src="iconSrc" />
+      <span v-if="title">{{ title }}</span>
     </div>
   </a>
 </template>
 
 <script>
 export default {
-  props: ['iconContainerClass', 'iconClass', 'iconSrc', 'title'],
-}
+  props: ["iconContainerClass", "iconClass", "iconSrc", "title"],
+};
 </script>
 
 <style>
+.icon {
+  transition: all 0.2s ease-in-out;
+}
+
+.icon:hover {
+  transform: scale(1.1);
+}
 </style>
