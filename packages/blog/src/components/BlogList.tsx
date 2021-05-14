aa65cd4432bd9d39b202d8712da97b862c586f02
Alexey Berezin <beraliv.spb@gmail.com>
2021-05-15 00:52:25 +0200

    refactor: 🔄 use BlogPost

--- packages/blog/src/components/BlogList.tsx
+++ packages/blog/src/components/BlogList.tsx
@@ -2,6 +2,7 @@ import { Link, PageProps } from "gatsby"
 import { GatsbyImage, IGatsbyImageData } from "gatsby-plugin-image"
 import React from "react"
 import { Bio } from "./Bio"
+import { BlogPost } from "./BlogPost"
 import { Layout } from "./Layout"
 import { Seo } from "./Seo"
 
@@ -10,7 +11,7 @@ interface Postable {
     slug?: string | null
   } | null
   frontmatter?: {
-    categories?: any[] | null
+    categories?: (string | null)[] | null
     description?: string | null
     image?: {
       childImageSharp?: {
@@ -52,17 +53,47 @@ export const BlogList = <T extends Postable>({
     )
   }
 
-  // let postsWithCategories = []
-  // let postsWithoutCategories = []
+  posts.forEach((post, index) => {
+    if (!post.fields?.slug) {
+      throw new Error(`Cannot extract slug for post #${index + 1}`)
+    }
 
-  // for (const post of posts) {
-  //   const length = post.frontmatter?.categories?.length ?? 0
-  //   if (length > 0) {
-  //     postsWithCategories.push(post)
-  //   } else {
-  //     postsWithoutCategories.push(post)
-  //   }
-  // }
+    if (!post.frontmatter) {
+      throw new Error(`Cannot find frontmatter in ${post.fields.slug}index.md`)
+    }
+
+    if (!post.frontmatter.title) {
+      throw new Error(`Cannot find title in ${post.fields.slug}index.md`)
+    }
+
+    if (!post.frontmatter.description) {
+      throw new Error(`Cannot find description in ${post.fields.slug}index.md`)
+    }
+  })
+
+  let postsWithoutCategories: T[] = []
+  let postsByCategories: Record<string, T[]> = {}
+
+  for (const post of posts) {
+    const categories = post.frontmatter?.categories ?? []
+    if (categories.length > 0) {
+      categories.forEach(category => {
+        if (!category) {
+          return
+        }
+
+        if (!postsByCategories[category]) {
+          postsByCategories[category] = []
+        }
+
+        postsByCategories[category].push(post)
+      })
+    } else {
+      postsWithoutCategories.push(post)
+    }
+  }
+
+  const categories = Object.keys(postsByCategories)
 
   return (
     <Layout location={location} title={title}>
@@ -70,57 +101,58 @@ export const BlogList = <T extends Postable>({
       <Bio />
       {tag && <h1>{`#${tag}`}</h1>}
       <ol>
-        {posts.map((post, index) => {
-          if (!post.fields?.slug) {
-            throw new Error(`Cannot extract slug for post #${index + 1}`)
-          }
+        {categories.map(category => {
+          const postsByCategory = postsByCategories[category]
 
-          if (!post.frontmatter) {
-            throw new Error(
-              `Cannot find frontmatter in ${post.fields.slug}index.md`
+          return postsByCategory.map(post => {
+            const { description, title, image } = post.frontmatter!
+
+            return (
+              <li className="post-list-item" key={post.fields!.slug}>
+                <article itemScope itemType="http://schema.org/Article">
+                  <header>
+                    <small>{post.frontmatter!.date}</small>
+                    <h2>
+                      <Link to={post.fields!.slug!} itemProp="url">
+                        <span itemProp="headline">{title}</span>
+                      </Link>
+                    </h2>
+                  </header>
+                  <section>
+                    {image?.childImageSharp?.gatsbyImageData && (
+                      <GatsbyImage
+                        image={image?.childImageSharp.gatsbyImageData}
+                        alt={title!}
+                      />
+                    )}
+                  </section>
+                  <footer>
+                    <p
+                      dangerouslySetInnerHTML={{
+                        __html: description!,
+                      }}
+                      itemProp="description"
+                    />
+                  </footer>
+                </article>
+              </li>
             )
-          }
+          })
+        })}
+        {postsWithoutCategories.map(post => {
+          const { description, title, image } = post.frontmatter!
 
-          if (!post.frontmatter.title) {
-            throw new Error(`Cannot find title in ${post.fields.slug}index.md`)
-          }
-
-          if (!post.frontmatter.description) {
-            throw new Error(
-              `Cannot find description in ${post.fields.slug}index.md`
-            )
-          }
-
-          const { description, title, image } = post.frontmatter
+          const slug = post.fields!.slug!
 
           return (
-            <li className="post-list-item" key={post.fields.slug}>
-              <article itemScope itemType="http://schema.org/Article">
-                <header>
-                  <small>{post.frontmatter.date}</small>
-                  <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                </header>
-                <section>
-                  {image?.childImageSharp?.gatsbyImageData && (
-                    <GatsbyImage
-                      image={image?.childImageSharp.gatsbyImageData}
-                      alt={title}
-                    />
-                  )}
-                </section>
-                <footer>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: description,
-                    }}
-                    itemProp="description"
-                  />
-                </footer>
-              </article>
+            <li className="post-list-item" key={slug}>
+              <BlogPost
+                date={post.frontmatter!.date!}
+                slug={slug}
+                gatsbyImageData={image?.childImageSharp?.gatsbyImageData}
+                title={title!}
+                html={description!}
+              />
             </li>
           )
         })}
