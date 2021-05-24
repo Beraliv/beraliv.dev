1c529687a6c12d6a0a36ae66e75027c1c9cb9e06
Alexey Berezin <beraliv.spb@gmail.com>
2021-05-24 20:03:39 +0200

    style: 💅 change Header

--- packages/blog/src/components/Layout.tsx
+++ packages/blog/src/components/Layout.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent } from "react"
-import { Link, PageProps } from "gatsby"
-import { DarkModeToggle } from "./DarkModeToggle"
-import { SubscriptionForm } from "./SubscriptionForm"
+import { PageProps } from "gatsby"
+// import { DarkModeToggle } from "./DarkModeToggle"
+// import { SubscriptionForm } from "./SubscriptionForm"
 
 declare const __PATH_PREFIX__: string
 
@@ -17,31 +17,23 @@ export const Layout: FunctionComponent<LayoutProps> = ({
 }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
-  let header
-
-  if (isRootPath) {
-    header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
-  }
 
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
       <header className="global-header">
-        {header}
-        <DarkModeToggle />
+        {isRootPath && (
+          <>
+            <h1 className="main-heading">{title}</h1>
+            <div className="main-description">
+              frontend developer based in Munich
+            </div>
+          </>
+        )}
+        {/* <DarkModeToggle /> */}
       </header>
-      <main>{children}</main>
-      <SubscriptionForm />
-      <footer>
+      <main className="global-main">{children}</main>
+      {/* <SubscriptionForm /> */}
+      <footer className="global-footer">
         © {new Date().getFullYear()}, Built with
         {` `}
         <a
