ba083a00db225d054ea0450da361e73af1a54905
Alexey Berezin <beraliv.spb@gmail.com>
2021-06-17 23:32:34 +0200

    build: 🔄 use @vercel/node for dev 📦

--- packages/blog/src/pages/api/views/[slug].ts
+++ packages/blog/src/pages/api/views/[slug].ts
@@ -1,4 +1,4 @@
-import { VercelRequest, VercelResponse } from "@vercel/node"
+import type { VercelRequest, VercelResponse } from "@vercel/node"
 import { firebaseDb } from "../../../api/firebaseDb"
 
 interface DataSnapshot {
