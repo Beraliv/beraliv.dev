47b7d54ba5302c0c33298166f6115f9200f266b0
Alexey Berezin <beraliv.spb@gmail.com>
2021-07-07 20:19:19 +0200

    test: 🧪 getPostBySlug

---
+++ packages/blog2/functions/getPostBySlug/index.test.ts
@@ -0,0 +1,43 @@
+import { getPostBySlug } from ".";
+
+describe(getPostBySlug.name, () => {
+  test("returns real data from 2018-10-04-scrollbar-customisation", () => {
+    const {
+      title,
+      date,
+      slug,
+      description,
+      labels,
+      keywords,
+      categories,
+      featured,
+    } = getPostBySlug("2018-10-04-scrollbar-customisation", [
+      "title",
+      "date",
+      "slug",
+      "description",
+      "labels",
+      "keywords",
+      "categories",
+      "featured",
+    ]);
+
+    expect(title).toBe<typeof title>(
+      "Scrollbar customisation in JavaScript and CSS"
+    );
+    expect(date).toBe<typeof date>("2018-10-04");
+    expect(slug).toBe<typeof slug>("2018-10-04-scrollbar-customisation");
+    expect(description).toBe<typeof description>(
+      "What is the current state of scrollbar customisation? Let's have a look at the browser capabilities, CSS hacks and JS libraries."
+    );
+    expect(labels).toEqual<typeof labels>(["css", "javascript"]);
+    expect(keywords).toEqual<typeof keywords>([
+      "scrollbar",
+      "customisation",
+      "javascript",
+      "css",
+    ]);
+    expect(categories).toEqual<typeof keywords>(undefined);
+    expect(featured).toEqual<typeof featured>("./macos-scrollbar.png");
+  });
+});
--- packages/blog2/functions/getPostBySlug/index.ts
+++ packages/blog2/functions/getPostBySlug/index.ts
@@ -2,14 +2,18 @@ import { readFileSync } from "fs";
 import { join } from "path";
 import frontmatter from "gray-matter";
 import { CONTENT_DIR } from "../../constants/CONTENT_DIR";
+import { PostType } from "../../types/PostType";
 
-export const getPostBySlug = (slug: string, fields: string[] = []) => {
+export const getPostBySlug = (
+  slug: string,
+  fields: (keyof PostType)[] = []
+): PostType => {
   const slugName = slug.replace(/\.md$/, "");
   const fullPath = join(CONTENT_DIR, `${slugName}.md`);
   const fileContents = readFileSync(fullPath, "utf8");
   const { data, content } = frontmatter(fileContents);
 
-  const items: Record<string, string> = {};
+  const items: Partial<PostType> = {};
   fields.forEach((field) => {
     if (field === "slug") {
       items[field] = slugName;
@@ -23,5 +27,5 @@ export const getPostBySlug = (slug: string, fields: string[] = []) => {
     }
   });
 
-  return items;
+  return items as PostType;
 };
