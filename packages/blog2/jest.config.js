bb8b01bbd1afb8fb3ca106985794ef6f9deb8a60
Alexey Berezin <beraliv.spb@gmail.com>
2021-07-07 20:18:42 +0200

    build: ➕ add jest 📦

---
+++ packages/blog2/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  roots: ["<rootDir>"],
+  moduleFileExtensions: ["ts", "tsx", "js", "json", "jsx"],
+  testPathIgnorePatterns: ["<rootDir>[/\\\\](node_modules|.next)[/\\\\]"],
+  transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "babel-jest",
+  },
+};
