8ac7fb8c1ec22c77d96db35edb57d8b234beb339
Alexey Berezin <beraliv.spb@gmail.com>
2021-07-15 23:19:26 +0200

    refactor: 🔄 simplify Header ⚛️

--- packages/blog2/src/components/molecules/Header/index.tsx
+++ packages/blog2/src/components/molecules/Header/index.tsx
@@ -1,40 +1,32 @@
 import React, { FC } from "react";
+import { validateNever } from "../../../validators/validateNever";
 import { DarkModeToggle } from "../../atoms/DarkModeToggle";
 import styles from "./index.module.css";
 
-declare const __PATH_PREFIX__: string;
-
 interface HeaderPropsType {
   title: string;
-  path?: "home" | "post";
+  path: "home" | "post";
 }
 
 export const Header: FC<HeaderPropsType> = ({ title, path }) => {
-  console.log(`>>> header path`, __PATH_PREFIX__);
-
-  const rootPath = `${__PATH_PREFIX__}/`;
-  const isRootPath = location.pathname === rootPath;
   let header;
 
-  if (isRootPath) {
+  if (path === "home") {
     header = (
       <h1 className={styles.atHome}>
-        <a href="/" target="_blank" rel="nofollow noopener noreferrer">
+        <a href="/" rel="nofollow noopener noreferrer">
           {title}
         </a>
       </h1>
     );
-  } else {
+  } else if (path === "post") {
     header = (
-      <a
-        className={styles.toHome}
-        href="/"
-        target="_blank"
-        rel="nofollow noopener noreferrer"
-      >
+      <a className={styles.toHome} href="/" rel="nofollow noopener noreferrer">
         {title}
       </a>
     );
+  } else {
+    validateNever(path);
   }
 
   return (
