3ea5f9a749ae87e63d5e6aff6f77007822563936
Alexey Berezin <beraliv.spb@gmail.com>
2021-07-15 13:33:29 +0200

    refactor: 🔄 use mdxComponents in PostBody

--- packages/blog2/src/components/atoms/PostBody.tsx
+++ packages/blog2/src/components/atoms/PostBody.tsx
@@ -1,13 +1,12 @@
 import { FC } from "react";
 import { MDXRemote } from "next-mdx-remote";
 import { PostPropsType } from "../pages/Post";
+import { mdxComponents } from "../mdx";
 
 export interface PostBodyPropsType {
   content: PostPropsType["content"];
 }
 
-const components = {};
-
 export const PostBody: FC<PostBodyPropsType> = ({ content }) => (
-  <MDXRemote {...content} components={components} />
+  <MDXRemote {...content} components={mdxComponents} />
 );
