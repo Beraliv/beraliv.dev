265d1a5fc0312c6a39e3dbf9a5f78fe1fdadae9f
Alexey Berezin <beraliv.spb@gmail.com>
2021-07-15 18:51:03 +0200

    style: ➕ add BuyMeCoffeeButton 💅

--- packages/blog2/src/components/atoms/BuyMeCoffeeButton/index.tsx
+++ packages/blog2/src/components/atoms/BuyMeCoffeeButton/index.tsx
@@ -1,10 +1,11 @@
 import React, { FC } from "react";
 import NextImage from "next/image";
 import butMyACoffeeIcon from "../../../../public/buy-me-a-coffee-icon.png";
+import styles from "./index.module.css";
 
 export const BuyMeCoffeeButton: FC = () => (
   <a
-    className="buymecoffee"
+    className={styles.buymecoffee}
     href="https://www.buymeacoffee.com/beraliv"
     target="_blank"
     rel="nofollow noopener noreferrer"
