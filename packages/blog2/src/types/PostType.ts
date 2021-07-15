b21ce6892b39ba77e19b64103a1d67113938311c
Alexey Berezin <beraliv.spb@gmail.com>
2021-07-15 16:44:19 +0200

    refactor: 🔄 make all keys of PostType required

--- packages/blog2/src/types/PostType.ts
+++ packages/blog2/src/types/PostType.ts
@@ -1,11 +1,11 @@
 export interface PostType {
-  title?: string;
-  date?: string;
-  slug?: string;
-  description?: string;
-  content?: string;
-  labels?: string[];
-  keywords?: string[];
-  categories?: string[];
-  featured?: string;
+  title: string;
+  date: string;
+  slug: string;
+  description: string;
+  content: string;
+  labels: string[];
+  keywords: string[];
+  categories: string[];
+  featured: string;
 }
