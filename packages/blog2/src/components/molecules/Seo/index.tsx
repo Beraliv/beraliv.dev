45ff542733561367edfa008160fa52034596a9a1
Alexey Berezin <beraliv.spb@gmail.com>
2021-07-16 16:18:18 +0200

    feat: ➕ add OpenGraphSeo ⚛️

--- packages/blog2/src/components/molecules/Seo/index.tsx
+++ packages/blog2/src/components/molecules/Seo/index.tsx
@@ -2,11 +2,10 @@ import NextHead from "next/head";
 import { FC } from "react";
 import { classNames } from "../../../functions/classNames";
 
-interface TwitterSeoPropsType {
-  description: string;
-  image: string;
-  title: string;
-}
+type TwitterSeoPropsType = Pick<
+  SeoPropsType,
+  "description" | "image" | "title"
+>;
 
 const TwitterSeo: FC<TwitterSeoPropsType> = ({ description, image, title }) => (
   <>
@@ -19,17 +18,48 @@ const TwitterSeo: FC<TwitterSeoPropsType> = ({ description, image, title }) => (
   </>
 );
 
+type OpenGraphSeoPropsType = Pick<
+  SeoPropsType,
+  "description" | "image" | "imageHeight" | "imageWidth" | "path" | "title"
+>;
+
+const OpenGraphSeo: FC<OpenGraphSeoPropsType> = ({
+  description,
+  image,
+  imageHeight,
+  imageWidth,
+  path,
+  title,
+}) => (
+  <>
+    <meta property="og:site_name" content="beraliv" />
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content={path} />
+    <meta property="og:image" content={image} />
+    <meta property="og:image:height" content={`${imageHeight}`} />
+    <meta property="og:image:width" content={`${imageWidth}`} />
+  </>
+);
+
 interface SeoPropsType {
   description: string;
   image: string;
+  imageHeight: number;
+  imageWidth: number;
   keywords: string[];
+  path: string;
   title: string;
 }
 
 export const Seo: FC<SeoPropsType> = ({
   description,
   image,
+  imageHeight,
+  imageWidth,
   keywords,
+  path,
   title,
 }) => {
   return (
@@ -38,6 +68,14 @@ export const Seo: FC<SeoPropsType> = ({
       <meta name="description" content={description} />
       <meta name="image" content={image} />
       <meta name="keywords" content={classNames(...keywords)} />
+      <OpenGraphSeo
+        description={description}
+        image={image}
+        imageHeight={imageHeight}
+        imageWidth={imageWidth}
+        path={path}
+        title={title}
+      />
       <TwitterSeo description={description} image={image} title={title} />
     </NextHead>
   );
