40df292ad51c01ab17361a30273d458836a80e9e
Alexey Berezin <beraliv.spb@gmail.com>
2021-07-17 00:28:41 +0200

    refactor: 🔄 does NOT scale image by default

--- packages/blog2/src/functions/imageLoader/index.test.ts
+++ packages/blog2/src/functions/imageLoader/index.test.ts
@@ -1,6 +1,13 @@
 import { imageLoader } from ".";
 
 describe(imageLoader.name, () => {
+  test("does not scale image by default", () => {
+    const actual = imageLoader({ src: "/testing/image.png" });
+    const expected = `https://res.cloudinary.com/beraliv/image/upload/v1626453732/blog_beraliv_dev/testing/image.png`;
+
+    expect(actual).toEqual(expected);
+  });
+
   test("use quality of 75 by default", () => {
     const actual = imageLoader({ src: "/testing/image.png", width: 125 });
     const expected = `https://res.cloudinary.com/beraliv/image/upload/f_auto,w_125,q_75/v1626453732/blog_beraliv_dev/testing/image.png`;
