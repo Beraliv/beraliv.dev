96238d1755c5eac2c0fb74458cb902fed8434fdf
Alex Berezin <beraliv.spb@gmail.com>
2021-07-16 15:41:15 +0200

    feat: ➕ add views serverless function 🖊 (#199)
    * feat: ➕ add ViewsApi
    
    * build: ➕ add firebase-admin 📦
    
    * feat: ➕ add firebaseDb
    
    * feat: ➕ add serverless function views 🖊

---
+++ packages/blog2/pages/api/views.ts
@@ -0,0 +1,50 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { firebaseDb } from "../../src/functions/firebaseDb";
+import { ViewsApi } from "../../src/types/ViewsApi";
+
+interface DataSnapshot {
+  val(): unknown;
+}
+
+const viewsRef = "beraliv-blog/views";
+
+export default async (
+  request: NextApiRequest,
+  response: NextApiResponse<ViewsApi>
+) => {
+  const { slug } = request.query;
+  if (typeof slug !== "string") {
+    return response.status(400);
+  }
+
+  if (request.method === "POST") {
+    // you cannot use destructuring here
+    const ref = firebaseDb.ref(viewsRef).child(slug);
+
+    const { snapshot } = await ref.transaction((views: unknown) => {
+      if (typeof views !== "number") {
+        return 1;
+      }
+
+      return views + 1;
+    });
+    // TODO: fix types
+    const views = (snapshot as DataSnapshot).val() as number;
+
+    const result: ViewsApi = { views };
+
+    return response.status(200).json(result);
+  }
+
+  if (request.method === "GET") {
+    // you cannot use destructuring here
+    const snapshot = await firebaseDb.ref(viewsRef).child(slug).once("value");
+
+    const views = snapshot.val();
+
+    return response.status(200).json({ views });
+  }
+
+  return response.status(404);
+};
