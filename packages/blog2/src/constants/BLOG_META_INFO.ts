6a06b3897c95e383ccd8923b12186bf5dc096ba2
Alexey Berezin <beraliv.spb@gmail.com>
2021-07-16 16:37:28 +0200

    feat: ➕ add BLOG_META_INFO

---
+++ packages/blog2/src/constants/BLOG_META_INFO.ts
@@ -0,0 +1,10 @@
+export const BLOG_META_INFO = {
+  title: "beraliv",
+  author: "Alexey Berezin",
+  summary: "who loves London 🏴󠁧󠁢󠁥󠁮󠁧󠁿, players ⏯ and TypeScript",
+  keywords: ["video players", "typescript"],
+  url: "blog2.beraliv.dev",
+  social: {
+    twitter: "beraliv",
+  },
+};
