3c7c6e8849df6f31209530b9c7decbe1492d68b9
Alexey Berezin <beraliv.spb@gmail.com>
2021-07-17 00:34:47 +0200

    style: 💅 MdxImage

--- packages/blog2/src/components/mdx/MdxImage/index.tsx
+++ packages/blog2/src/components/mdx/MdxImage/index.tsx
@@ -1,6 +1,7 @@
 import NextImage from "next/image";
 import { FC } from "react";
 import { imageLoader } from "../../../functions/imageLoader";
+import styles from "./index.module.css";
 
 interface MxdImagePropsType {
   alt: string;
@@ -10,7 +11,7 @@ interface MxdImagePropsType {
 }
 
 export const MdxImage: FC<MxdImagePropsType> = (props) => (
-  <figure>
+  <figure className={styles.figure}>
     <NextImage {...props} loader={imageLoader} />
     <figcaption>{props.alt}</figcaption>
   </figure>
