5e76bf60fd589927d6505ed2e75f1b06063ce7ba
Alexey Berezin <beraliv.spb@gmail.com>
2021-07-17 12:54:42 +0200

    feat: ➕ add initial script in _document

--- packages/blog2/pages/_document.tsx
+++ packages/blog2/pages/_document.tsx
@@ -28,6 +28,29 @@ class ExtendedDocument extends Document {
           />
         </Head>
         <body>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `;(function () {
+                var STORAGE_KEY = "dark"
+                var CLASSNAME = "dark"
+              
+                var INITIAL_DARK_MODE = (() => {
+                  try {
+                    var persistedDarkMode = localStorage.getItem(STORAGE_KEY)
+                    if (persistedDarkMode === null) {
+                      return false
+                    }
+              
+                    return JSON.parse(persistedDarkMode)
+                  } catch (error) {
+                    return false
+                  }
+                })()
+              
+                document.body.classList.toggle(CLASSNAME, INITIAL_DARK_MODE)
+              })();`,
+            }}
+          />
           <Main />
           <NextScript />
         </body>
