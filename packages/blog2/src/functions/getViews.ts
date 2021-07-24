c5e9bc8aaf7cbd2e9bd01942922795104f455624
Alexey Berezin <beraliv.spb@gmail.com>
2021-07-24 22:51:08 +0200

    refactor: 🔄 make views number

--- packages/blog2/src/functions/getViews.ts
+++ packages/blog2/src/functions/getViews.ts
@@ -1,18 +1,21 @@
 import { FIREBASE_VIEWS_REF } from "../constants/FIREBASE_VIEWS_REF";
+import { ViewsApi } from "../types/ViewsApi";
 import { firebaseDb } from "./firebaseDb";
 
 interface GetViewsPropsType {
   slug: string;
 }
 
-export const getViews = async ({ slug }: GetViewsPropsType) => {
+export const getViews = async ({
+  slug,
+}: GetViewsPropsType): Promise<ViewsApi> => {
   // you cannot use destructuring here
   const snapshot = await firebaseDb
     .ref(FIREBASE_VIEWS_REF)
     .child(slug)
     .once("value");
 
-  const views = snapshot.val();
+  const views = snapshot.val() as number;
 
   return { views };
 };
