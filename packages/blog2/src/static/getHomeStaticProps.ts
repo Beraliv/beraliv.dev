11322d56d0fc1647aba375d997aa54e4c4e04fe0
Alexey Berezin <beraliv.spb@gmail.com>
2021-07-24 22:51:20 +0200

    refactor: 🔄 getHomeStaticProps

--- packages/blog2/src/static/getHomeStaticProps.ts
+++ packages/blog2/src/static/getHomeStaticProps.ts
@@ -4,53 +4,44 @@ import { validateEnvParameters } from "../validators/validateEnvParameters";
 import { validatePost } from "../validators/validatePost";
 
 export const getHomeStaticProps = async () => {
-  const posts = getAllPosts();
-  const { apiKey, formId } = validateEnvParameters();
-
-  const featuredPosts = posts
-    .filter((post) => post.data.featured)
-    .map(({ data, name }) =>
-      validatePost({
-        ...data,
-        slug: name,
-      })
-    );
-
-  const latestPosts = posts
-    .map(({ data, name }) =>
-      validatePost({
-        ...data,
-        slug: name,
-      })
-    )
+  const posts = getAllPosts()
+    .map(({ data, name }) => ({
+      ...data,
+      slug: name,
+    }))
     .sort((a, b) => {
       if (!a.date) return -1;
       if (!b.date) return 1;
       return -a.date.localeCompare(b.date);
     })
-    .slice(0, 6);
+    .map(validatePost);
 
-  const readablePosts = await Promise.all(
-    posts.map(async ({ data, name }) => {
-      const slug = name;
+  const featuredPosts = posts.filter((post) => post.featured);
+
+  const latestPosts = posts.filter((post) => !post.featured).slice(0, 6);
+
+  const postsWithViews = await Promise.all(
+    posts.map(async ({ slug, ...post }) => {
       const { views } = await getViews({ slug });
 
       return {
-        ...data,
+        ...post,
         slug,
         views,
       };
     })
   );
 
-  const mostViewedPosts = readablePosts
+  const mostViewedPosts = postsWithViews
     .sort((a, b) => {
       if (!a.views) return -1;
       if (!b.views) return 1;
       return b.views - a.views;
     })
     .slice(0, 6)
-    .map(({ views, ...uncheckedPost }) => validatePost(uncheckedPost));
+    .map(({ views, ...uncheckedPost }) => uncheckedPost);
+
+  const { apiKey, formId } = validateEnvParameters();
 
   return {
     props: {
