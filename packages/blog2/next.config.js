61e9b2b8e8c71845379c585633d10baed0686d8b
Alexey Berezin <beraliv.spb@gmail.com>
2021-07-25 21:03:07 +0200

    feat: ➕ add 4 blog posts from twitter

--- packages/blog2/next.config.js
+++ packages/blog2/next.config.js
@@ -1,3 +1,27 @@
 module.exports = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/2021/06/16/append-to-object",
+        destination: "/2021-06-16-append-to-object",
+        permanent: true,
+      },
+      {
+        source: "/2021/06/21/absolute-in-typescript",
+        destination: "/2021-06-21-absolute-in-typescript",
+        permanent: true,
+      },
+      {
+        source: "/2021/06/19/making-union-out-of-string",
+        destination: "/2021-06-19-making-union-out-of-string",
+        permanent: true,
+      },
+      {
+        source: "/2021/07/05/spread-in-typescript",
+        destination: "/2021-07-05-spread-in-typescript",
+        permanent: true,
+      },
+    ];
+  },
+};
