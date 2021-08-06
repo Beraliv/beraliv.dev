3a2e3b920c63e1c9829701006507cd1c0d435433
Alexey Berezin <beraliv.spb@gmail.com>
2021-08-06 19:35:06 +0200

    feat: ➕ add TickIcon ⚛️

---
+++ packages/blog2/src/components/atoms/TickIcon/index.tsx
@@ -0,0 +1,13 @@
+import styles from "./index.module.css";
+
+export const TickIcon = () => (
+  <svg
+    width="25"
+    height="25"
+    viewBox="0 0 25 25"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M20 6L9 17L4 12" className={styles.path} />
+  </svg>
+);
