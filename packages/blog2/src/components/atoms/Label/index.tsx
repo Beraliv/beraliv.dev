f4ac4e3e2ec7901cf57e1134791761c68b0f5a46
Alexey Berezin <beraliv.spb@gmail.com>
2021-08-15 22:01:08 +0200

    feat: ➕ add selected for Label

--- packages/blog2/src/components/atoms/Label/index.tsx
+++ packages/blog2/src/components/atoms/Label/index.tsx
@@ -1,12 +1,19 @@
 import React, { FC } from "react";
+import { classNames } from "../../../functions/classNames";
 import styles from "./index.module.css";
 
 interface LabelPropsType {
+  selected?: boolean;
   title: string;
 }
 
-export const Label: FC<LabelPropsType> = ({ title }) => (
-  <a className={styles.label} href={`/search?label=${title}`}>
+export const Label: FC<LabelPropsType> = ({ selected = false, title }) => (
+  <a
+    className={classNames(styles.label, {
+      [styles.selected]: selected,
+    })}
+    href={selected ? `/search` : `/search?label=${title}`}
+  >
     {title}
   </a>
 );
