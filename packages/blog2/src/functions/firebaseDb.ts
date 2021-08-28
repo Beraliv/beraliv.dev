f9f9ad8dd71e90ffd9ca50a58281cd0243da55f2
Alex Berezin <beraliv.spb@gmail.com>
2021-08-28 07:57:27 +0200

    feat: 🚀 exactOptionalPropertyTypes: true (#242)
    * build: ➕ add exactOptionalPropertyTypes: true
    
    * fix: 🧪 type
    
    * fix: 🧪 views
    
    * build: 🔄 sort keys in compilerOptions

--- packages/blog2/src/functions/firebaseDb.ts
+++ packages/blog2/src/functions/firebaseDb.ts
@@ -1,11 +1,22 @@
 import admin from "firebase-admin";
 
+const privateKey = process.env.FIREBASE_PRIVATE_KEY;
+const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
+
+if (!privateKey) {
+  throw new Error(`Cannot find private key in env parameters`);
+}
+
+if (!clientEmail) {
+  throw new Error(`Cannot find client email in env parameters`);
+}
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: "digital-garden-ffb02-default-rtdb",
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey,
+      clientEmail,
     }),
     databaseURL:
       "https://digital-garden-ffb02-default-rtdb.europe-west1.firebasedatabase.app/",
