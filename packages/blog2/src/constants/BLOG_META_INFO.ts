0dc9c5213366cf4fa7839fb3f6f434931c940e08
Alexey Berezin <beraliv.spb@gmail.com>
2021-10-05 01:31:04 +0200

    test: 🔄 update BLOG_META_INFO url

--- packages/blog2/src/constants/BLOG_META_INFO.ts
+++ packages/blog2/src/constants/BLOG_META_INFO.ts
@@ -3,7 +3,7 @@ export const BLOG_META_INFO = {
   author: "Alexey Berezin",
   summary: "who loves London 🏴󠁧󠁢󠁥󠁮󠁧󠁿, players ⏯ and TypeScript",
   keywords: ["video players", "typescript"],
-  url: "https://blog2.beraliv.dev/",
+  url: "https://blog2-git-fix-seo-in-head-testing-beraliv.vercel.app/",
   social: {
     twitter: "beraliv",
   },
