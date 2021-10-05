647f74668a791d5c73d4db34bf044c7994f310be
Alexey Berezin <beraliv.spb@gmail.com>
2021-10-05 16:44:19 +0200

    feat: ➕ add PROMO_IMAGE

---
+++ packages/blog2/src/constants/PROMO_IMAGE.ts
@@ -0,0 +1,7 @@
+import { ImageType } from "../types/ImageType";
+
+export const PROMO_IMAGE: ImageType = {
+  url: "https://res.cloudinary.com/beraliv/image/upload/v1633444777/blog_beraliv_dev/banner.png",
+  width: 1600,
+  height: 900,
+};
