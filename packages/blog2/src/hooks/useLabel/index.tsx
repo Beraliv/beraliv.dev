2afe4007a9dfe4eb7a67c3dd5d9cc53f00181d42
Alex Berezin <beraliv.spb@gmail.com>
2021-10-11 22:23:16 +0200

    fix: 🧪 useIsomorphicLayoutEffect (#275)
    * feat: ➕ add useIsomorphicLayoutEffect ⚛️
    
    * refactor: 🔄 use useIsomorphicLayoutEffect

--- packages/blog2/src/hooks/useLabel/index.tsx
+++ packages/blog2/src/hooks/useLabel/index.tsx
@@ -1,7 +1,8 @@
-import { useLayoutEffect, useState } from "react";
+import { useState } from "react";
 import { getLocationSearchParameters } from "../../functions/getLocationSearchParameters";
 import { LabelType } from "../../types/LabelType";
 import { validateLabel } from "../../validators/validateLabel";
+import { useIsomorphicLayoutEffect } from "../useIsomorphicLayoutEffect";
 
 type LabelReturnType =
   | {
@@ -15,7 +16,7 @@ type LabelReturnType =
 export const useLabel = () => {
   const [state, setState] = useState<LabelReturnType>({ state: "loading" });
 
-  useLayoutEffect(() => {
+  useIsomorphicLayoutEffect(() => {
     const { label } = getLocationSearchParameters(location.search);
     if (!label) {
       setState({ state: "loaded", value: undefined });
