a0d98b142c6a878e897b4b44c3e690d7c924bfb0
Alexey Berezin <beraliv.spb@gmail.com>
2021-10-14 11:32:52 +0200

    feat: ➕ add getPostUtcDate

---
+++ packages/blog2/src/functions/getPostUtcDate/index.test.ts
@@ -0,0 +1,16 @@
+import { getPostUtcDate } from "./";
+
+describe(getPostUtcDate.name, () => {
+  test("returns milliseconds for correct date", () => {
+    const actual = getPostUtcDate({ date: "2021-03-26" });
+    const expected = 1616716800000;
+
+    expect(actual).toEqual(expected);
+  });
+
+  test("throws error if date cannot be parsed", () => {
+    expect(() => getPostUtcDate({ date: "" })).toThrow(
+      /Post date format is incorrect: /
+    );
+  });
+});
