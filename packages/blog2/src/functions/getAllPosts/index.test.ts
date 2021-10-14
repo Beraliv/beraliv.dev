f450d5f49eaa5b3f3aa1c66c5dc7a7d15a329199
Alexey Berezin <beraliv.spb@gmail.com>
2021-10-14 12:07:37 +0200

    test: ➕ add posts in getAllPosts

--- packages/blog2/src/functions/getAllPosts/index.test.ts
+++ packages/blog2/src/functions/getAllPosts/index.test.ts
@@ -2,7 +2,8 @@ import { getAllPosts } from ".";
 
 describe(getAllPosts.name, () => {
   test("returns all posts from content directory", () => {
-    const slugs = getAllPosts().map(({ name }) => name);
+    const posts = getAllPosts();
+    const slugs = posts.map(({ name }) => name);
 
     expect(slugs).toEqual<typeof slugs>([
       "2018-10-04-scrollbar-customisation",
