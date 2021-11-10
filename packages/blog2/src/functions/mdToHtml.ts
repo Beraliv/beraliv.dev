5d100bfa2a4de107d230b085b9a0a51231fed16a
Alexey Berezin <beraliv.spb@gmail.com>
2021-11-10 21:46:29 +0100

    feat: 🔄 use remarkUnwrapImages

--- packages/blog2/src/functions/mdToHtml.ts
+++ packages/blog2/src/functions/mdToHtml.ts
@@ -1,7 +1,12 @@
 import { remark } from "remark";
-import html from "remark-html";
+import remarkHtml from "remark-html";
+import remarkUnwrapImages from "remark-unwrap-images";
 
 export const mdToHtml = async (md: string) => {
-  const result = await remark().use(html).process(md);
+  const result = await remark()
+    .use(remarkUnwrapImages)
+    .use(remarkHtml)
+    .process(md);
+
   return result.toString();
 };
