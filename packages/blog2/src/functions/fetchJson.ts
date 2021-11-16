438041a1320b0ac19e1f5ef4ef85b0eaef33a3e1
Alexey Berezin <beraliv.spb@gmail.com>
2021-11-16 23:13:18 +0100

    feat: ➕ add Promise<T> to fetchJson

--- packages/blog2/src/functions/fetchJson.ts
+++ packages/blog2/src/functions/fetchJson.ts
@@ -1,5 +1,7 @@
-export const fetchJson = async (...args: Parameters<typeof fetch>) => {
+export const fetchJson = async <T>(
+  ...args: Parameters<typeof fetch>
+): Promise<T> => {
   const response = await fetch(...args);
-  const json = await response.json();
+  const json: T = await response.json();
   return json;
 };
