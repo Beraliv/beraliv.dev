5810aaeb02e15392aa5c4a61b771815438da7a33
Alex Berezin <beraliv.spb@gmail.com>
2021-11-16 19:42:39 +0100

    build: 🔄 use remark-unwrap-images 📦 (#299)
    * build: ➕ add remark-unwrap-images 📦
    
    * feat: 🔄 use remarkUnwrapImages
    
    * refactor: ❌ remove useless mdToHtml
    
    * feat: ➕ add remarkUnwrapImages
    
    * build: ➕ add incremental build
    
    * style: 💅 fix figure margin-bottom

--- packages/blog2/src/functions/mdToHtml.ts
+++
@@ -1,7 +0,0 @@
-import { remark } from "remark";
-import html from "remark-html";
-
-export const mdToHtml = async (md: string) => {
-  const result = await remark().use(html).process(md);
-  return result.toString();
-};
