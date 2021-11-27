a691ec30b478c7d6c2709756be98663bd2ab4963
Alexey Berezin <beraliv.spb@gmail.com>
2021-11-27 16:45:54 +0100

    fix: 🧪 meta info: blog2 => blog

--- packages/blog2/src/plugins/rssMetadata/generateRssXmlItem/index.ts
+++ packages/blog2/src/plugins/rssMetadata/generateRssXmlItem/index.ts
@@ -4,11 +4,11 @@ import { sanitiseHtml } from "../../../functions/sanitiseHtml";
 import { ValidatedPostType } from "../../../types/ValidatedPostType";
 import { toUtcString } from "../toUtcString";
 
-const { author } = BLOG_META_INFO;
+const { author, url } = BLOG_META_INFO;
 
 export const generateRssXmlItem = (post: ValidatedPostType): string => {
   const utcTimestamp = getPostUtcDate(post);
-  const link = `https://blog2.beraliv.dev/${post.slug}`;
+  const link = `${url}${post.slug}`;
 
   return `<item>
             <title><![CDATA[${sanitiseHtml(post.title)}]]></title>
