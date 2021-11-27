dfc45db591111929530ff2398e0a39344b9994bf
Alexey Berezin <beraliv.spb@gmail.com>
2021-11-27 16:50:25 +0100

    test: 🧪 generateRssXml

--- packages/blog2/src/plugins/rssMetadata/generateRssXml/index.test.ts
+++ packages/blog2/src/plugins/rssMetadata/generateRssXml/index.test.ts
@@ -31,21 +31,21 @@ describe(generateRssXml.name, () => {
     <channel>
         <title><![CDATA[Alexey Berezin blog]]></title>
         <description><![CDATA[Hi I'm Alexey Berezin, who loves London 🏴󠁧󠁢󠁥󠁮󠁧󠁿, players ⏯ and TypeScript.]]></description>
-        <link>https://blog2.beraliv.dev/</link>
+        <link>https://blog.beraliv.dev/</link>
         <image>
             <url>https://res.cloudinary.com/beraliv/image/upload/v1633444777/blog_beraliv_dev/banner.png</url>
             <title>Alexey Berezin blog</title>
-            <link>https://blog2.beraliv.dev/</link>
+            <link>https://blog.beraliv.dev/</link>
         </image>
         <generator>RSS for Node</generator>
         <lastBuildDate>Thu, 04 Oct 2018 00:00:00 GMT</lastBuildDate>
-        <atom:link href="https://blog2.beraliv.dev/rss.xml" rel="self" type="application/rss+xml"/>
+        <atom:link href="https://blog.beraliv.dev/rss.xml" rel="self" type="application/rss+xml"/>
         <language><![CDATA[en]]></language>
         <item>
             <title><![CDATA[Scrollbar customisation]]></title>
             <description><![CDATA[CSS support in different browser engines, hacks and tricks, JS libraries]]></description>
-            <link>https://blog2.beraliv.dev/2018-10-04-scrollbar-customisation</link>
-            <guid isPermaLink="false">https://blog2.beraliv.dev/2018-10-04-scrollbar-customisation</guid>
+            <link>https://blog.beraliv.dev/2018-10-04-scrollbar-customisation</link>
+            <guid isPermaLink="false">https://blog.beraliv.dev/2018-10-04-scrollbar-customisation</guid>
             <dc:creator><![CDATA[Alexey Berezin]]></dc:creator>
             <pubDate>Thu, 04 Oct 2018 00:00:00 GMT</pubDate>
         </item>
@@ -61,29 +61,29 @@ describe(generateRssXml.name, () => {
     <channel>
         <title><![CDATA[Alexey Berezin blog]]></title>
         <description><![CDATA[Hi I'm Alexey Berezin, who loves London 🏴󠁧󠁢󠁥󠁮󠁧󠁿, players ⏯ and TypeScript.]]></description>
-        <link>https://blog2.beraliv.dev/</link>
+        <link>https://blog.beraliv.dev/</link>
         <image>
             <url>https://res.cloudinary.com/beraliv/image/upload/v1633444777/blog_beraliv_dev/banner.png</url>
             <title>Alexey Berezin blog</title>
-            <link>https://blog2.beraliv.dev/</link>
+            <link>https://blog.beraliv.dev/</link>
         </image>
         <generator>RSS for Node</generator>
         <lastBuildDate>Tue, 17 Dec 2019 00:00:00 GMT</lastBuildDate>
-        <atom:link href="https://blog2.beraliv.dev/rss.xml" rel="self" type="application/rss+xml"/>
+        <atom:link href="https://blog.beraliv.dev/rss.xml" rel="self" type="application/rss+xml"/>
         <language><![CDATA[en]]></language>
         <item>
             <title><![CDATA[Scrollbar customisation]]></title>
             <description><![CDATA[CSS support in different browser engines, hacks and tricks, JS libraries]]></description>
-            <link>https://blog2.beraliv.dev/2018-10-04-scrollbar-customisation</link>
-            <guid isPermaLink="false">https://blog2.beraliv.dev/2018-10-04-scrollbar-customisation</guid>
+            <link>https://blog.beraliv.dev/2018-10-04-scrollbar-customisation</link>
+            <guid isPermaLink="false">https://blog.beraliv.dev/2018-10-04-scrollbar-customisation</guid>
             <dc:creator><![CDATA[Alexey Berezin]]></dc:creator>
             <pubDate>Thu, 04 Oct 2018 00:00:00 GMT</pubDate>
         </item>
         <item>
             <title><![CDATA[Video player in Amazon]]></title>
             <description><![CDATA[From video tag to the implementation, state machine, the way bundle is loaded, bundle names]]></description>
-            <link>https://blog2.beraliv.dev/2019-12-17-amazon-prime-video-player-investigation</link>
-            <guid isPermaLink="false">https://blog2.beraliv.dev/2019-12-17-amazon-prime-video-player-investigation</guid>
+            <link>https://blog.beraliv.dev/2019-12-17-amazon-prime-video-player-investigation</link>
+            <guid isPermaLink="false">https://blog.beraliv.dev/2019-12-17-amazon-prime-video-player-investigation</guid>
             <dc:creator><![CDATA[Alexey Berezin]]></dc:creator>
             <pubDate>Tue, 17 Dec 2019 00:00:00 GMT</pubDate>
         </item>
