c92ad1ccdd233de62f8dbfa67eb9ead7e0dca53d
Alexey Berezin <beraliv.spb@gmail.com>
2021-11-28 00:09:18 +0100

    fix: 🧪 preload image metadata if missed (#311)
    * feat: ➕ add SizeInformation and extractMetadata
    
    * refactor: 🔄 simplify imageMetadata
    
    * refactor: 🔄 call extractMetadata if no imageUrl in cache

---
+++ packages/blog2/src/functions/extractMetadata/index.ts
@@ -0,0 +1,60 @@
+import { readFile, writeFile } from "fs";
+import { join } from "path";
+import { promisify } from "util";
+import { SizeInformation } from "../../types/SizeInformation";
+import { fetchJson } from "../fetchJson";
+import { sizeLoader } from "../sizeLoader";
+import { imageLoader } from "../imageLoader";
+import { DeepPartial } from "../../types/DeepPartial";
+
+const readFileAsync = promisify(readFile);
+const writeFileAsync = promisify(writeFile);
+
+const validateSize = (
+  information: DeepPartial<SizeInformation> | undefined
+): SizeInformation["output"] => {
+  if (!information) {
+    throw new Error(`Invalid object in size information`);
+  }
+
+  if (!information.output) {
+    throw new Error(`Invalid output in size information`);
+  }
+
+  const { width, height } = information.output;
+
+  if (!width) {
+    throw new Error(`Invalid width in size information`);
+  }
+
+  if (!height) {
+    throw new Error(`INvalid height in size information`);
+  }
+
+  return { width, height };
+};
+
+export async function extractMetadata(
+  src: string
+): Promise<SizeInformation["output"]> {
+  const cachePath = join(process.cwd(), "src", "cache", "imageMetadata.json");
+  const rawCache = await readFileAsync(cachePath, "utf8");
+  const cache = JSON.parse(rawCache);
+
+  if (cache[src]) {
+    const { width, height } = cache[src];
+
+    return { width, height };
+  }
+
+  const imageUrl = imageLoader({ src });
+  const sizeUrl = sizeLoader({ src: imageUrl });
+  const sizeInformation: SizeInformation = await fetchJson(sizeUrl);
+  const { width, height } = validateSize(sizeInformation);
+
+  cache[src] = { width, height };
+  const updatedCache = JSON.stringify(cache, null, 2);
+  await writeFileAsync(cachePath, updatedCache);
+
+  return { width, height };
+}
---
+++ packages/blog2/src/types/SizeInformation.ts
@@ -0,0 +1,6 @@
+export interface SizeInformation {
+  output: {
+    width: number;
+    height: number;
+  };
+}
