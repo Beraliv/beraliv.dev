cd7ec3d384a8300c7f86af336a117105918b57d5
Alexey Berezin <beraliv.spb@gmail.com>
2022-01-19 00:59:07 +0100

    fix: ➕ add key for snowflakes

--- packages/blog2/src/components/atoms/Snow/index.tsx
+++ packages/blog2/src/components/atoms/Snow/index.tsx
@@ -7,10 +7,7 @@ interface SnowPropsType {
 export const Snow = ({ number }: SnowPropsType) => {
   const snowflakes = Array(number)
     .fill(0)
-    .map(() => (
-      // eslint-disable-next-line react/jsx-key
-      <div className={styles.snowflake} />
-    ));
+    .map((_, index) => <div key={index} className={styles.snowflake} />);
 
   return <div className={styles.snow}>{snowflakes}</div>;
 };
