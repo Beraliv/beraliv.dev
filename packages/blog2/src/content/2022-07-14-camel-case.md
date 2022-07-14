a072f8821df8ce62a0e695166cf8a83aa08cd392
Alexey Berezin <beraliv.spb@gmail.com>
2022-07-14 22:17:07 +0100

    refactor: 🔄 2022-07-(10 => 14)-camel-case

--- packages/blog2/src/content/2022-07-14-camel-case.md
+++ packages/blog2/src/content/2022-07-14-camel-case.md
@@ -1,6 +1,6 @@
 ---
 title: CamelCase
-date: "2022-07-10"
+date: "2022-07-14"
 description: Given a string in any case, returns a string in camel case
 labels:
   - typescript
--- packages/blog2/src/functions/getAllPosts/index.test.ts
+++ packages/blog2/src/functions/getAllPosts/index.test.ts
@@ -54,7 +54,7 @@ describe(getAllPosts.name, () => {
       "2021-12-03-string-to-number",
       "2021-12-07-get-optional",
       "2021-12-10-advanced-types-holyjs-notes",
-      "2022-07-10-camel-case",
+      "2022-07-14-camel-case",
     ]);
   });
 });
