869d3caf436899756b72319fc97ebfafaa4be6b9
Alexey Berezin <beraliv.spb@gmail.com>
2022-11-24 22:12:17 +0000

    style: 💅 update HabitsPage
    1. update PlusIcon
    
    2. add hover cursor for checkbox and
    CreateHabitButton
    
    3. make checkbox and CreateHabitButton larger
    
    4.
    Adjust Habit card margins

--- packages/habit/src/components/atoms/PlusIcon/index.tsx
+++ packages/habit/src/components/atoms/PlusIcon/index.tsx
@@ -1,50 +1,22 @@
 import { Component } from "solid-js";
 
+import styles from "./index.module.css";
+
 const PlusIcon: Component = () => (
   <svg
-    version="1.1"
-    id="Layer_1"
     xmlns="http://www.w3.org/2000/svg"
-    x="0px"
-    y="0px"
-    width="32px"
-    height="32px"
-    viewBox="0 0 64 64"
-    enable-background="new 0 0 64 64"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="white"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class={styles.icon}
+    height="18"
+    width="18"
   >
-    <g>
-      <line
-        fill="none"
-        stroke="#000000"
-        stroke-width="2"
-        stroke-miterlimit="10"
-        x1="32"
-        y1="50"
-        x2="32"
-        y2="14"
-      />
-      <line
-        fill="none"
-        stroke="#000000"
-        stroke-width="2"
-        stroke-miterlimit="10"
-        x1="14"
-        y1="32"
-        x2="50"
-        y2="32"
-      />
-    </g>
-    <g>
-      <circle
-        fill="none"
-        stroke="#000000"
-        stroke-width="2"
-        stroke-miterlimit="10"
-        cx="32"
-        cy="32"
-        r="30.999"
-      />
-    </g>
+    <line x1="12" y1="5" x2="12" y2="19"></line>
+    <line x1="5" y1="12" x2="19" y2="12"></line>
   </svg>
 );
 
