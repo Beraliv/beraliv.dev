456f9fb6d8d19916511f4ea4ecbc52840c16c76e
Alexey Berezin <beraliv.spb@gmail.com>
2022-11-24 09:31:27 +0000

    feat: ➕ add habits
    1. edit and submit habit
    
    2. render habits
    
    3. complete habit

--- packages/habit/src/App.tsx
+++ packages/habit/src/App.tsx
@@ -1,22 +1,55 @@
-import { Component } from "solid-js";
+import { batch, Component, createSignal, For } from "solid-js";
+import { createStore } from "solid-js/store";
 
 import styles from "./App.module.css";
 
+interface Habit {
+  title: string;
+  completed: boolean;
+}
+
 const App: Component = () => {
+  const [habitTitle, setHabitTitle] = createSignal("");
+  const [habits, setHabits] = createStore<Habit[]>([]);
+
+  const addHabit = (e: SubmitEvent) => {
+    e.preventDefault();
+
+    batch(() => {
+      setHabits(habits.length, {
+        title: habitTitle(),
+        completed: false,
+      });
+      setHabitTitle("");
+    });
+  };
+
   return (
     <div class={styles.App}>
       <header class={styles.header}>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+        <form onSubmit={addHabit}>
+          <input
+            placeholder="Add a habit"
+            required
+            value={habitTitle()}
+            onInput={(e) => setHabitTitle(e.currentTarget.value)}
+          />
+          <button>+</button>
+        </form>
+        <For each={habits}>
+          {(habit, index) => (
+            <div class={styles.habit}>
+              <input
+                type="checkbox"
+                checked={habit.completed}
+                onChange={(e) =>
+                  setHabits(index(), "completed", e.currentTarget.checked)
+                }
+              />
+              <div>{habit.title}</div>
+            </div>
+          )}
+        </For>
       </header>
     </div>
   );
