f4da34534d48b9622907841a8ee3d92fd913469e
Alexey Berezin <beraliv.spb@gmail.com>
2022-11-24 09:09:40 +0000

    refactor: ❌ remove logo ⚛️

--- packages/habit/src/App.tsx
+++ packages/habit/src/App.tsx
@@ -1,13 +1,11 @@
-import type { Component } from 'solid-js';
+import { Component } from "solid-js";
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+import styles from "./App.module.css";
 
 const App: Component = () => {
   return (
     <div class={styles.App}>
       <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
