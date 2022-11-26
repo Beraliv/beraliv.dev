470df0970e11792c9dfa7e2234f5e0d1464f3452
Alexey Berezin <beraliv.spb@gmail.com>
2022-11-26 17:49:14 +0000

    feat: ➕ add PickIcon ⚛️

--- packages/habit/src/components/molecules/IconPopup.tsx
+++ packages/habit/src/components/molecules/IconPopup.tsx
@@ -1,5 +1,6 @@
 import { Component, createSignal, Show, Signal } from "solid-js";
 import { TIconType } from "../../types/TIconType";
+import { PickIcon } from "../atoms/PickIcon";
 import { Popup } from "../atoms/Popup";
 import { IconButton } from "./IconButton";
 
@@ -20,6 +21,14 @@ const IconPopup: Component<IIconPopupProps> = ({ iconSignal }) => {
       <Show when={isPopupVisible()}>
         <Popup handleCancel={closePopup} handleSave={closePopup}>
           <h3 class={styles.h3}>Icon</h3>
+          <div class={styles.grid}>
+            <PickIcon type="faceSmile" signal={iconSignal} />
+            <PickIcon type="handsBubbles" signal={iconSignal} />
+            <PickIcon type="heartPulse" signal={iconSignal} />
+            <PickIcon type="language" signal={iconSignal} />
+            <PickIcon type="paw" signal={iconSignal} />
+            <PickIcon type="personRunning" signal={iconSignal} />
+          </div>
         </Popup>
       </Show>
     </>
