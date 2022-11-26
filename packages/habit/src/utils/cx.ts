944863de1df2886b437a41852bfa7bb9ec9d46b8
Alexey Berezin <beraliv.spb@gmail.com>
2022-11-26 01:24:43 +0000

    refactor: 🔄 update cx for objects

--- packages/habit/src/utils/cx.ts
+++ packages/habit/src/utils/cx.ts
@@ -1,3 +1,19 @@
-const cx = (...classes: string[]) => classes.join(" ");
+const cx = (...classes: (string | Record<string, boolean>)[]) => {
+  const filteredClasses = [];
+
+  for (const c of classes) {
+    if (typeof c === "string") {
+      filteredClasses.push(c);
+    } else if (typeof c === "object") {
+      for (const [key, value] of Object.entries(c)) {
+        if (value) {
+          filteredClasses.push(key);
+        }
+      }
+    }
+  }
+
+  return filteredClasses.join(" ");
+};
 
 export { cx };
