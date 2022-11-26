7aba21bba8398f686068cb42f4bb24946d7debff
Alexey Berezin <2991847+Beraliv@users.noreply.github.com>
2022-11-26 20:37:37 +0000

    fix: 🧪 create habits pages not found (#528)
    * feat: ➕ add lazy import
    
    * feat: ➕ add create.html for a router
    
    * build: 🔄 create.html => create/index.html

--- packages/habit/src/App.tsx
+++ packages/habit/src/App.tsx
@@ -1,17 +1,29 @@
-import { Component } from "solid-js";
+import { Component, lazy } from "solid-js";
 import { Route, Routes } from "@solidjs/router";
 
 import styles from "./App.module.css";
-import { HabitsPage } from "./components/pages/HabitsPage";
-import { CreateHabitPage } from "./components/pages/CreateHabitPage";
 
 const App: Component = () => {
   return (
     <div class={styles.App}>
       <header class={styles.header}>
         <Routes>
-          <Route path="/create" component={CreateHabitPage} />
-          <Route path="/" component={HabitsPage} />
+          <Route
+            path="/create"
+            component={lazy(() =>
+              import("./components/pages/CreateHabitPage").then((module) => ({
+                default: module.CreateHabitPage,
+              }))
+            )}
+          />
+          <Route
+            path="/"
+            component={lazy(() =>
+              import("./components/pages/HabitsPage").then((module) => ({
+                default: module.HabitsPage,
+              }))
+            )}
+          />
         </Routes>
       </header>
     </div>
