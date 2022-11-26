c908a4de86c69bf1dab2885498148d83fdadb26b
Alexey Berezin <beraliv.spb@gmail.com>
2022-11-26 17:55:10 +0000

    refactor: ❌ remove CreateHabitButton folder

--- packages/habit/src/components/molecules/CreateHabitButton.tsx
+++ packages/habit/src/components/molecules/CreateHabitButton.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "@solidjs/router";
 import { Component } from "solid-js";
-import { PlusIcon } from "../../atoms/PlusIcon";
+import { PlusIcon } from "../atoms/PlusIcon";
 
 const CreateHabitButton: Component = () => {
   const navigate = useNavigate();
