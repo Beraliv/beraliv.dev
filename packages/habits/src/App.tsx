1625af4046489774214034ff91c4027fe5e3bc75
Alexey Berezin <beraliv.spb@gmail.com>
2022-12-04 21:16:59 +0000

    refactor: ❌ remove sign in on mount

--- packages/habits/src/App.tsx
+++ packages/habits/src/App.tsx
@@ -1,9 +1,7 @@
-import { Component, lazy, onMount } from "solid-js";
+import { Component, lazy } from "solid-js";
 import { Route, Routes } from "@solidjs/router";
 
 import styles from "./App.module.css";
-import { $googleSso, signIn } from "./state/google-sso";
-import { useUnit } from "effector-solid";
 import { UserProfileHeader } from "./components/molecules/UserProfileHeader";
 
 const CreateHabitPage = lazy(() =>
@@ -19,10 +17,6 @@ const HabitsPage = lazy(() =>
 );
 
 const App: Component = () => {
-  onMount(() => {
-    signIn();
-  });
-
   return (
     <div class={styles.App}>
       <UserProfileHeader />
