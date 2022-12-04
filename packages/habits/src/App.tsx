6f94ad216e8ce95ac4a4f3ca0982176ba99a5c6d
Alexey Berezin <beraliv.spb@gmail.com>
2022-12-04 15:28:54 +0000

    refactor: 🔄 move lazy page loading to separate variables

--- packages/habits/src/App.tsx
+++ packages/habits/src/App.tsx
@@ -3,27 +3,25 @@ import { Route, Routes } from "@solidjs/router";
 
 import styles from "./App.module.css";
 
+const CreateHabitPage = lazy(() =>
+  import("./components/pages/CreateHabitPage").then((module) => ({
+    default: module.CreateHabitPage,
+  }))
+);
+
+const HabitsPage = lazy(() =>
+  import("./components/pages/HabitsPage").then((module) => ({
+    default: module.HabitsPage,
+  }))
+);
+
 const App: Component = () => {
   return (
     <div class={styles.App}>
       <header class={styles.header}>
         <Routes>
-          <Route
-            path="/create"
-            component={lazy(() =>
-              import("./components/pages/CreateHabitPage").then((module) => ({
-                default: module.CreateHabitPage,
-              }))
-            )}
-          />
-          <Route
-            path="/"
-            component={lazy(() =>
-              import("./components/pages/HabitsPage").then((module) => ({
-                default: module.HabitsPage,
-              }))
-            )}
-          />
+          <Route path="/create" component={CreateHabitPage} />
+          <Route path="/" component={HabitsPage} />
         </Routes>
       </header>
     </div>
