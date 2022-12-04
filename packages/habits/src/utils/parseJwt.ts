604225247123390623e188a4949d2ebe022a37a9
Alexey Berezin <beraliv.spb@gmail.com>
2022-12-04 20:54:58 +0000

    feat: ➕ add parseJwt

---
+++ packages/habits/src/utils/parseJwt.ts
@@ -0,0 +1,16 @@
+import { TPrimitive } from "../types/TPrimitive";
+
+const parseJwt = (token: string): Record<string, TPrimitive> => {
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map((c) => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2))
+      .join("")
+  );
+
+  return JSON.parse(jsonPayload);
+};
+
+export { parseJwt };
