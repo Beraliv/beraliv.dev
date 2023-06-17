190c9b4d8f3d8eef8c17fe2958aa488311dc582c
Alexey Berezin <beraliv.spb@gmail.com>
2023-06-18 00:51:10 +0100

    style: 🖼️ fix images

--- packages/tennis-tournaments/src/Constants/TENNIS_PLAYER_IMG_URL_MAPPING.ts
+++
@@ -1,8 +0,0 @@
-// TODO: move it to CDN, e.g. Cloudflare, and remove
-const TENNIS_PLAYER_IMG_URL_MAPPING: Record<string, string> = {
-  fritz: "/src/assets/fritz.avif",
-  nadal: "/src/assets/nadal.avif",
-  djokovic: "/src/assets/djokovic.avif",
-};
-
-export { TENNIS_PLAYER_IMG_URL_MAPPING };
--- packages/tennis-tournaments/src/Utils/createTennisPlayerFromTeam.ts
+++ packages/tennis-tournaments/src/Utils/createTennisPlayerFromTeam.ts
@@ -1,4 +1,3 @@
-import { TENNIS_PLAYER_IMG_URL_MAPPING } from "../Constants/TENNIS_PLAYER_IMG_URL_MAPPING";
 import { Api } from "../Types/Api";
 import { TennisPlayer } from "../Types/TennisPlayer";
 
@@ -8,7 +7,7 @@ const createTennisPlayerFromTeam = (team: Team): TennisPlayer => {
   // Kyrgios N. => [Kyrgios, N.]
   const [lastName, firstName] = team.name.split(" ").map((name) => name.trim());
 
-  const imageUrl = TENNIS_PLAYER_IMG_URL_MAPPING[lastName.toLowerCase()] || "";
+  const imageUrl = `/src/assets/${lastName.toLowerCase()}.avif`;
 
   return {
     country: "",
