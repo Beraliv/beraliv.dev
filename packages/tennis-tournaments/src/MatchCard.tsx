e335bbc4c54701d3a5947fab14f066f807c3f43b
Alexey Berezin <beraliv.spb@gmail.com>
2023-06-18 13:12:20 +0100

    refactor: 🔄 simplify MatchCard

--- packages/tennis-tournaments/src/MatchCard.tsx
+++ packages/tennis-tournaments/src/MatchCard.tsx
@@ -1,13 +1,9 @@
-import { For, type Component, Show } from "solid-js";
+import { type Component } from "solid-js";
 
 import styles from "./MatchCard.module.css";
 import { TennisPlayer } from "./Types/TennisPlayer";
-import { createShortName } from "./Utils/createShortName";
-import winnerIcon from "./Icons/Winner.svg";
 import { TennisSet } from "./Types/TennisSet";
-import { hasTieBreak } from "./Utils/hasTieBreak";
-import { doesWinSet } from "./Utils/doesWinSet";
-import { classNames } from "./Utils/classNames";
+import { PlayerMatchResult } from "./PlayerMatchResult";
 
 interface MatchCardProps {
   awayPlayer: TennisPlayer;
@@ -22,104 +18,25 @@ const MatchCard: Component<MatchCardProps> = ({
   sets,
   winner,
 }) => {
-  const homeShortName = createShortName(homePlayer);
-  const awayShortName = createShortName(awayPlayer);
-
-  const doesHomeWinMatch = winner === "home";
-  const doesAwayWinMatch = winner === "away";
+  const homeCentricSets = sets;
+  const awayCentricSets: TennisSet[] = sets.map(([opponentScore, score]) => [
+    score,
+    opponentScore,
+  ]);
 
   return (
     <div class={styles.MatchCard}>
-      <div class={styles.Home}>
-        <div class={styles.Player}>
-          <div>
-            <img class={styles.AvatarImage} src={homePlayer.imageUrl} />
-          </div>
-          <div
-            class={classNames({
-              [styles.WinHighlighter]: doesHomeWinMatch,
-            })}
-          >
-            {homeShortName}
-          </div>
-          <div class={styles.Ranking}>{`(${homePlayer.ranking})`}</div>
-        </div>
-        <div class={styles.Score}>
-          <Show when={doesHomeWinMatch}>
-            <div class={styles.Winner}>
-              <img class={styles.WinnerImage} src={winnerIcon} />
-            </div>
-          </Show>
-          <div class={styles.SetScores}>
-            <For each={sets}>
-              {([firstItem, secondItem]) => (
-                <div
-                  class={classNames(styles.SetScore, {
-                    [styles.WinHighlighter]: doesWinSet([
-                      firstItem,
-                      secondItem,
-                    ]),
-                  })}
-                >
-                  {firstItem.games}
-                  <Show when={hasTieBreak(firstItem)}>
-                    {
-                      <sup class={styles.TieBreakScore}>
-                        {hasTieBreak(firstItem) && firstItem.tieBreak}
-                      </sup>
-                    }
-                  </Show>
-                </div>
-              )}
-            </For>
-          </div>
-        </div>
-      </div>
-      <div class={styles.Away}>
-        <div class={styles.Player}>
-          <div>
-            <img class={styles.AvatarImage} src={awayPlayer.imageUrl} />
-          </div>
-          <div
-            class={classNames({
-              [styles.WinHighlighter]: doesAwayWinMatch,
-            })}
-          >
-            {awayShortName}
-          </div>
-          <div class={styles.Ranking}>{`(${awayPlayer.ranking})`}</div>
-        </div>
-        <div class={styles.Score}>
-          <Show when={doesAwayWinMatch}>
-            <div class={styles.Winner}>
-              <img class={styles.WinnerImage} src={winnerIcon} />
-            </div>
-          </Show>
-          <div class={styles.SetScores}>
-            <For each={sets}>
-              {([firstItem, secondItem]) => (
-                <div
-                  class={classNames(styles.SetScore, {
-                    [styles.WinHighlighter]: doesWinSet([
-                      secondItem,
-                      firstItem,
-                    ]),
-                  })}
-                >
-                  {secondItem.games}
-                  <Show when={hasTieBreak(secondItem)}>
-                    {
-                      <sup class={styles.TieBreakScore}>
-                        {hasTieBreak(secondItem) && secondItem.tieBreak}
-                      </sup>
-                    }
-                  </Show>
-                </div>
-              )}
-            </For>
-          </div>
-        </div>
-      </div>
+      <PlayerMatchResult
+        className={styles.Home}
+        doesWinMatch={winner === "home"}
+        player={homePlayer}
+        playerCentricSets={homeCentricSets}
+      />
+      <PlayerMatchResult
+        doesWinMatch={winner === "away"}
+        player={awayPlayer}
+        playerCentricSets={awayCentricSets}
+      />
     </div>
   );
 };
