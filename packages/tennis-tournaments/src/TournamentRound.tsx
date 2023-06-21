b69e560246e092ee67afd52ab4c90f14808923c5
Alexey Berezin <beraliv.spb@gmail.com>
2023-06-21 09:36:35 +0100

    feat: ➕ add TournamentRound ⚛️

---
+++ packages/tennis-tournaments/src/TournamentRound.tsx
@@ -0,0 +1,43 @@
+import { type Component, For } from "solid-js";
+
+import styles from "./TournamentRound.module.css";
+import { MatchCard, MatchCardProps } from "./MatchCard";
+import { classNames } from "./Utils/classNames";
+
+interface TournamentRoundProps {
+  matches: MatchCardProps[];
+  order: number;
+  title: string;
+}
+
+const TournamentRound: Component<TournamentRoundProps> = ({
+  matches,
+  order,
+  title,
+}) => {
+  return (
+    <div
+      class={classNames(styles.TournamentRound, {
+        [styles.Order0]: order === 0,
+        [styles.Order1]: order === 1,
+        [styles.Order2]: order === 2,
+      })}
+    >
+      <h1>{title}</h1>
+      <For each={matches}>
+        {(match) => (
+          <div class={styles.CardWrapper}>
+            <MatchCard
+              awayPlayer={match.awayPlayer}
+              homePlayer={match.homePlayer}
+              sets={match.sets}
+              winner={match.winner}
+            />
+          </div>
+        )}
+      </For>
+    </div>
+  );
+};
+
+export { TournamentRound };
