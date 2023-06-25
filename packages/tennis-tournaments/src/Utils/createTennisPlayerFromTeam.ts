0f7b97a992a4f469515decacfe56b3bf7119442c
Alexey Berezin <beraliv.spb@gmail.com>
2023-06-25 14:33:22 +0100

    refactor: 🔄 update first and last names

--- packages/tennis-tournaments/src/Utils/createTennisPlayerFromTeam.ts
+++ packages/tennis-tournaments/src/Utils/createTennisPlayerFromTeam.ts
@@ -1,9 +1,15 @@
 import { TeamApiModel } from "../Types/TeamApiModel";
 import { TennisPlayer } from "../Types/TennisPlayer";
 
+const NAME_SEPARATOR = " ";
+
 const createTennisPlayerFromTeam = (team: TeamApiModel): TennisPlayer => {
+  // de Minaur A. => [de, Minaur, A.]
   // Kyrgios N. => [Kyrgios, N.]
-  const [lastName, firstName] = team.name.split(" ").map((name) => name.trim());
+  const names = team.name.split(NAME_SEPARATOR).map((name) => name.trim());
+
+  const firstName = names.at(-1) || "";
+  const lastName = names.slice(0, -1).join(NAME_SEPARATOR);
 
   const imageUrl = `https://res.cloudinary.com/beraliv/image/upload/v1687048409/tennis_tournaments/players/${lastName.toLowerCase()}.avif`;
 
