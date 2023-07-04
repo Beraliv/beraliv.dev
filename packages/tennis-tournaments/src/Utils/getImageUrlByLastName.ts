dcce1d8496560e018c23daa000e2899386f16509
Alexey Berezin <beraliv.spb@gmail.com>
2023-07-04 22:54:32 +0100

    refactor: 🔄 reuse same parts of getImageUrlByLastName

--- packages/tennis-tournaments/src/Utils/getImageUrlByLastName.ts
+++ packages/tennis-tournaments/src/Utils/getImageUrlByLastName.ts
@@ -1,12 +1,15 @@
 import { TENNIS_PLAYER_HASH_MAP } from "../Constants/TENNIS_PLAYER_HASH_MAP";
 import { TENNIS_PLAYER_PLACEHOLDER_VERSION } from "../Constants/TENNIS_PLAYER_PLACEHOLDER_VERSION";
 
+const CLOUDINARY_BASE_URL = "https://res.cloudinary.com/beraliv/image/upload";
+const CLOUDINARY_PATH = "tennis_tournaments/players";
+
 const getImageUrlByLastName = (slug: string): string => {
   if (TENNIS_PLAYER_HASH_MAP[slug]) {
-    return `https://res.cloudinary.com/beraliv/image/upload/w_275,ar_1:1,c_crop,g_face,r_max/v${TENNIS_PLAYER_HASH_MAP[slug]}/tennis_tournaments/players/${slug}.avif`;
+    return `${CLOUDINARY_BASE_URL}/w_275,ar_1:1,c_crop,g_face,r_max/v${TENNIS_PLAYER_HASH_MAP[slug]}/${CLOUDINARY_PATH}/${slug}.avif`;
   }
 
-  return `https://res.cloudinary.com/beraliv/image/upload/w_100,ar_1:1,c_fill,g_face,r_max/v${TENNIS_PLAYER_PLACEHOLDER_VERSION}/tennis_tournaments/players/placeholder.avif`;
+  return `${CLOUDINARY_BASE_URL}/w_100,ar_1:1,c_fill,g_face,r_max/v${TENNIS_PLAYER_PLACEHOLDER_VERSION}/${CLOUDINARY_PATH}/placeholder.avif`;
 };
 
 export { getImageUrlByLastName };
