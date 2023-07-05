d6a994f39eb1870916e597aac1b2600f7ca65551
Alexey Berezin <beraliv.spb@gmail.com>
2023-07-05 22:11:51 +0100

    feat: ➕ add grand slam tournaments

--- packages/tennis-tournaments/src/TournamentsPage.tsx
+++ packages/tennis-tournaments/src/TournamentsPage.tsx
@@ -7,13 +7,46 @@ const TournamentsPage: Component = () => {
     <div class={styles.TournamentsPage}>
       <TournamentCard
         courtType="hard"
+        // Jan 16 - Jan 29, 2023
+        tournamentPeriod={{
+          start: new Date(2023, 0, 16),
+          end: new Date(2023, 0, 29),
+        }}
+        tournamentId={2363}
+        tournamentName="Australian Open"
+        place="Melbourne, Australia"
+      />
+      <TournamentCard
+        courtType="clay"
+        // May 20 - June 9, 2023
+        tournamentPeriod={{
+          start: new Date(2023, 4, 20),
+          end: new Date(2023, 5, 9),
+        }}
+        tournamentId={2480}
+        tournamentName="Roland Garros"
+        place="Paris, France"
+      />
+      <TournamentCard
+        courtType="grass"
         // Jul 3 - Jul 16, 2023
         tournamentPeriod={{
-          start: new Date(2023, 6, 2),
-          end: new Date(2023, 6, 15),
+          start: new Date(2023, 6, 3),
+          end: new Date(2023, 6, 16),
         }}
-        tournamentId="2361"
-        tournamentName="The Championships, Wimbledon"
+        tournamentId={2361}
+        tournamentName="Wimbledon"
+        place="Wimbledon, UK"
+      />
+      <TournamentCard
+        courtType="hard"
+        // Aug 28 - Sep 10, 2023
+        tournamentPeriod={{
+          start: new Date(2023, 7, 28),
+          end: new Date(2023, 8, 10),
+        }}
+        tournamentId={2449}
+        tournamentName="US Open"
         place="Wimbledon, UK"
       />
     </div>
