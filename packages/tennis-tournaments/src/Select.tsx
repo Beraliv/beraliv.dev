43981ae601750e1cc6b58f5cb95ee68f7555bd0e
Alexey Berezin <beraliv.spb@gmail.com>
2023-09-04 23:17:01 +0100

    style: 💅 Select

--- packages/tennis-tournaments/src/Select.tsx
+++ packages/tennis-tournaments/src/Select.tsx
@@ -2,6 +2,7 @@ import { Accessor, For } from "solid-js";
 
 import styles from "./Select.module.css";
 
+import ArrowDownIcon from "./Icons/ArrowDown.svg";
 import { getObjectEntries } from "./Utils/getObjectEntries";
 
 type AnyEntries = Record<string, any>;
@@ -41,23 +42,32 @@ const Select = <Entries extends AnyEntries, Value extends string>({
     : getObjectEntries(data);
 
   return (
-    <div class={styles.Select}>
-      <label for={id}>{label}</label>
+    <div class={styles.Wrapper}>
+      <label class={styles.Label} for={id}>
+        {label}
+      </label>
 
-      <select
-        id={id}
-        name={label}
-        onChange={(event) => handleChange(event.target.value as Value)}
-        value={current()}
-      >
-        <For each={options}>
-          {([key, value]) => (
-            <option id={key as string} value={key as string}>
-              {value}
-            </option>
-          )}
-        </For>
-      </select>
+      <div class={styles.SelectWrapper}>
+        <select
+          class={styles.Select}
+          id={id}
+          name={label}
+          onChange={(event) => handleChange(event.target.value as Value)}
+          value={current()}
+        >
+          <For each={options}>
+            {([key, value]) => (
+              <option id={key as string} value={key as string}>
+                {value}
+              </option>
+            )}
+          </For>
+        </select>
+
+        <span class={styles.Icon}>
+          <ArrowDownIcon />
+        </span>
+      </div>
     </div>
   );
 };
