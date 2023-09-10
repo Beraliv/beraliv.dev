3c07e6c6528639298ffbffcbf7d61137924fea63
Alexey Berezin <beraliv.spb@gmail.com>
2023-09-10 23:59:06 +0100

    build: ➕ add vitest 📦

--- packages/tennis-tournaments/vite.config.ts
+++ packages/tennis-tournaments/vite.config.ts
@@ -1,4 +1,5 @@
 /// <reference types="./vite-env-override.d.ts" />
+/// <reference types="vitest" />
 import { defineConfig, loadEnv } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import solidSvg from "vite-plugin-solid-svg";
