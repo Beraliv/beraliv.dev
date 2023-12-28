111906f6492a9af16975548d6130dc886f87ac57
Alexey Berezin <beraliv.spb@gmail.com>
2023-12-28 23:57:31 +0000

    feat: ➕ add MdxYouTube ⚛️

---
+++ packages/blog2/src/components/mdx/MdxYouTube/index.tsx
@@ -0,0 +1,20 @@
+import styles from "./index.module.css";
+
+type MdxYouTubeProps = {
+  title: string;
+  href: string;
+};
+
+export const MdxYouTube = ({ href, title }: MdxYouTubeProps) => {
+  return (
+    <div className={styles.wrapper}>
+      <iframe
+        className={styles.youtubeIframe}
+        src={href}
+        title={title}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      />
+    </div>
+  );
+};
