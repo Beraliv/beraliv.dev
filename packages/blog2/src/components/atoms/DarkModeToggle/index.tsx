81a6e4a0dc015f3f2f30854a1e3ed7a9b7ccac29
Alexey Berezin <beraliv.spb@gmail.com>
2024-02-20 01:37:48 +0000

    style: 🔄 change moon/sun elements

--- packages/blog2/src/components/atoms/DarkModeToggle/index.tsx
+++ packages/blog2/src/components/atoms/DarkModeToggle/index.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { useDarkMode } from "../../../hooks/useDarkMode";
 import styles from "./index.module.css";
+import { classNames } from "../../../functions/classNames";
 
 export const DarkModeToggle = () => {
   const { darkMode, toggle } = useDarkMode();
 
   return (
-    <input
-      className={styles.toggle}
-      type="button"
-      value={darkMode ? "☀" : "☾"}
+    <div
+      className={classNames(styles.toggle, {
+        [styles.moon]: darkMode,
+        [styles.sun]: !darkMode,
+      })}
       onClick={toggle}
-    />
+    ></div>
   );
 };
