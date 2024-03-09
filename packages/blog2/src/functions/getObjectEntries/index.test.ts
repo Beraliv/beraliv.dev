a21cf336481504bfb53413cb14957ff2f332f00d
Alexey Berezin <beraliv.spb@gmail.com>
2024-03-09 21:51:06 +0000

    feat: ➕ getObjectEntries

---
+++ packages/blog2/src/functions/getObjectEntries/index.test.ts
@@ -0,0 +1,19 @@
+import { expect } from "earljs";
+import { getObjectEntries } from ".";
+
+describe(getObjectEntries.name, () => {
+  it("returns empty array for empty object", () => {
+    expect(getObjectEntries({})).toEqual([]);
+  });
+
+  it("returns one pair for object with one key", () => {
+    expect(getObjectEntries({ key: "value" })).toEqual([["key", "value"]]);
+  });
+
+  it("returns pairs for object with more than one key", () => {
+    expect(getObjectEntries({ key1: "value1", key2: "value2" })).toEqual([
+      ["key1", "value1"],
+      ["key2", "value2"],
+    ]);
+  });
+});
