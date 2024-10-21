d024fe8a807b7700fe79f98c939e9501e5881df7
Alexey Berezin <beraliv.spb@gmail.com>
2024-10-22 00:20:22 +0100

    feat: page lazy loading

--- packages/id/src/index.ts
+++ packages/id/src/index.ts
@@ -1,20 +1,19 @@
-import { aboutPage } from "./components/page-about";
-import { blogPage } from "./components/page-blog";
-import { page404 } from "./components/page-404";
 import "./style.css";
 
 const routes = {
-  "/": aboutPage,
-  "/404": page404,
-  "/blog": blogPage,
+  "/": async () => (await import("./components/page-about")).aboutPage,
+  "/404": async () => (await import("./components/page-404")).page404,
+  "/blog": async () => (await import("./components/page-blog")).blogPage,
 };
 
 type Route = keyof typeof routes;
 
-const router = () => {
+const router = async () => {
   const app = document.querySelector<HTMLDivElement>("#app")!;
   const route = window.location.pathname as Route;
-  app.innerHTML = routes[route] || routes["/404"];
+  const loadPage: () => Promise<string> = routes[route] || routes["/404"];
+  const page = await loadPage();
+  app.innerHTML = page;
 };
 
 window.addEventListener("load", router);
