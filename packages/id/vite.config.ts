c66dbd2a6956d2c870797418185c4cf21eef54c3
Alexey Berezin <beraliv.spb@gmail.com>
2024-11-04 23:58:56 +0000

    style: cache images based on approx. deployment time

--- packages/id/vite.config.ts
+++ packages/id/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { msToSeconds } from "./src/utils/msToSeconds";
+
+const BUILD_TIME = msToSeconds(Date.now());
+const DEPLOYMENT_TIME = BUILD_TIME + 10 * 60;
 
 export default defineConfig({
+  define: {
+    BUILD_TIME,
+    DEPLOYMENT_TIME,
+  },
   esbuild: {
     jsxFactory: "h",
     jsxFragment: "Fragment",
