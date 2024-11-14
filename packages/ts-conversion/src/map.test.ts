0169018ae76314aa21a3bc143870f4cfbf629729
Alexey Berezin <beraliv.spb@gmail.com>
2024-11-14 21:30:30 +0000

    test: map

---
+++ packages/ts-conversion/src/map.test.ts
@@ -0,0 +1,21 @@
+import { describe, expect, test } from "vitest";
+import { map } from "./map";
+
+describe("map", () => {
+  test("checks all combinations", () => {
+    const allCombinations = Object.values(map).flatMap((value) =>
+      Object.values(value)
+    );
+
+    expect(allCombinations.length).toBe(36);
+  });
+
+  test("checks implemented combinations", () => {
+    const allCombinations = Object.values(map).flatMap((value) =>
+      Object.values(value)
+    );
+    const implementedCombinations = allCombinations.filter(Boolean);
+
+    expect(implementedCombinations.length).toBe(10);
+  });
+});
