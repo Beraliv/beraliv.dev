0b4ec2849e6d63023af3c9b895331c0e5795897a
Alexey Berezin <beraliv.spb@gmail.com>
2025-04-01 01:37:10 +0100

    feat (id): blog publisher

--- packages/id/src/components/pages/Blog.tsx
+++ packages/id/src/components/pages/Blog.tsx
@@ -3,12 +3,16 @@ import { Header } from "../Header";
 import { Link, TextLinkProps } from "../Link";
 
 type ArticleProps = TextLinkProps & {
+  publisher: string;
   time: string;
 };
 
-const Article = ({ href, text, external, time }: ArticleProps) => (
+const Article = ({ href, text, external, time, publisher }: ArticleProps) => (
   <article>
-    <Link href={href} text={text} external={external} />
+    <div>
+      <Link href={href} text={text} external={external} />
+      <p className="publisher">{publisher}</p>
+    </div>
     <time>{time}</time>
   </article>
 );
@@ -33,12 +37,14 @@ const MainContent = () => (
         <Article
           href="https://hackernoon.com/the-scrollbar-customization-in-css-and-js-2024-update"
           text="The Scrollbar Customization in CSS and JS (2024 Update)"
+          publisher="HackerNoon"
           external
           time="Jun 07"
         />
         <Article
           href="https://habr.com/ru/articles/808015/"
           text="What if Get Function in Lodash inferred types for you in TypeScript (in Russian)"
+          publisher="Habr"
           external
           time="Apr 16"
         />
@@ -51,6 +57,7 @@ const MainContent = () => (
         <Article
           href="https://blog.beraliv.dev/2023-12-21-story-of-unknown-low-tier-device-and-its-mse-issues-lvt-notes"
           text="A story of an unknown low-tier device and its MSE issues / London Video Tech 2023 notes"
+          publisher="blog.beraliv.dev"
           external
           time="Dec 21"
         />
@@ -63,12 +70,14 @@ const MainContent = () => (
         <Article
           href="https://blog.beraliv.dev/2022-09-10-with-or-without-enums"
           text="With or without enums in TypeScript"
+          publisher="blog.beraliv.dev"
           external
           time="Sep 10"
         />
         <Article
           href="https://blog.beraliv.dev/2022-07-14-camel-case"
           text="Transform string literal type into camelCase in TypeScript"
+          publisher="blog.beraliv.dev"
           external
           time="Jul 14"
         />
@@ -81,18 +90,21 @@ const MainContent = () => (
         <Article
           href="https://blog.beraliv.dev/2021-12-10-advanced-types-holyjs-notes"
           text="Advanced types / Holy.js 2021 notes"
+          publisher="blog.beraliv.dev"
           external
           time="Dec 10"
         />
         <Article
           href="https://blog.beraliv.dev/2021-05-07-opaque-type-in-typescript"
           text="Opaque Types in TypeScript"
+          publisher="blog.beraliv.dev"
           external
           time="May 07"
         />
         <Article
           href="https://blog.beraliv.dev/2021-03-26-typed-get"
           text="Type-safe get function that extracts the value by paths in TypeScript"
+          publisher="blog.beraliv.dev"
           external
           time="Mar 26"
         />
@@ -105,6 +117,7 @@ const MainContent = () => (
         <Article
           href="https://beraliv.medium.com/research-joyn-scripts-obfuscation-d33b0e64f86d"
           text="Research Joyn scripts obfuscation"
+          publisher="Medium"
           external
           time="May 04"
         />
@@ -117,6 +130,7 @@ const MainContent = () => (
         <Article
           href="https://beraliv.medium.com/bbc-iplayer-geolocation-identification-636830e720d0"
           text="BBC iPlayer geolocation identification"
+          publisher="Medium"
           external
           time="Dec 22"
         />
