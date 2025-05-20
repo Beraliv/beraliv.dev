595fd403696b7ff559fe61aa131b31deb8a0390e
Alexey Berezin <beraliv.spb@gmail.com>
2025-05-20 21:52:44 +0100

    feat: definition link text & url

--- packages/video-glossary/src/Definition.tsx
+++ packages/video-glossary/src/Definition.tsx
@@ -2,7 +2,10 @@ export interface IDefinition {
   title: string;
   summary: string;
   description: string;
-  link: string;
+  link: {
+    text: string;
+    url: string;
+  };
 }
 
 export const Definition = (definition: IDefinition) => (
@@ -10,8 +13,13 @@ export const Definition = (definition: IDefinition) => (
     <h3>{definition.title}</h3>
     {definition.summary && <p className="summary">{definition.summary}</p>}
     <p className="description">{definition.description}</p>
-    <a className="link" href={definition.link}>
-      {definition.link}
+    <a
+      className="link"
+      href={definition.link.url}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {definition.link.text}
     </a>
   </div>
 );
