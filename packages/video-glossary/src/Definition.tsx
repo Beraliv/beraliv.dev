6e80119157af74d10b329b042528491f0ee1411a
Alexey Berezin <beraliv.spb@gmail.com>
2025-05-20 21:16:55 +0100

    feat: Definition

--- packages/video-glossary/src/Definition.tsx
+++ packages/video-glossary/src/Definition.tsx
@@ -6,9 +6,12 @@ export interface IDefinition {
 }
 
 export const Definition = (definition: IDefinition) => (
-  <a href={definition.link} className="search-result">
+  <div className="definition">
     <h3>{definition.title}</h3>
-    <p className="summary">{definition.summary}</p>
+    {definition.summary && <p className="summary">{definition.summary}</p>}
     <p className="description">{definition.description}</p>
-  </a>
+    <a className="link" href={definition.link}>
+      {definition.link}
+    </a>
+  </div>
 );
