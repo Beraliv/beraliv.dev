0c8dba01aeb4821f864d3a0980f9d1c2d7b7abab
Alexey Berezin <2991847+Beraliv@users.noreply.github.com>
2025-12-06 02:00:51 +0000

    refactor: move svg to separate files

---
+++ packages/achievements/src/components/PauseIcon.tsx
@@ -0,0 +1,11 @@
+export const PauseIcon = () => (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M6 4h4v16H6V4zm8 0h4v16h-4V4z" />
+  </svg>
+);
